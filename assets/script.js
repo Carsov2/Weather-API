f6231a91928a88bf75de09bb6f37db369d144763
Carson Kennedy <carsonkennedy2018@gmail.com>
2023-05-10 23:24:23 -0500

    landing page

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+// var APIKey = "70dbb2a7d07b8c1ca5346bfa22a84fe5"
+
+let findCity = "#findCity"
+let pastSearches = "#pastSearches"
+let cityDetails = "#cityDetails"
+let day1 = "#day1"
+let day2 = "#day2"
+let day3 = "#day3"
+let day4 = "#day4"
+let day5 = "#day5"
+let dayjs = day.js()
+
+const currentDate = dayjs().format('DD-MM-YYYY');
+console.log(currentDate);
+
+const date1 = dayjs().add(1, '#day').format('DD-MM-YYYY');
+const date2 = dayjs().add(2, '#day').format('DD-MM-YYYY');
+const date3 = dayjs().add(3, '#day').format('DD-MM-YYYY');
+const date4 = dayjs().add(4, '#day').format('DD-MM-YYYY');
+const date5 = dayjs().add(5, '#day').format('DD-MM-YYYY');
+
